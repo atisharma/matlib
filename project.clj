81ceff01baefdc21028b1a6fb2f496bdb1aa81cc
Ati <asharma@jupiter>
2020-07-14 11:52:06 +0100

    add clojure dep for codox

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject matlib "0.1.2"
+(defproject matlib "0.1.5"
   :description "A Clojure library of optimisation and control theory tools and convenience functions based on Neanderthal."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -17,12 +17,11 @@
   ; jvm-opts required by Neanderthal on JDK > 8.
   :jvm-opts ^:replace ["--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [uncomplicate/neanderthal "0.31.0"]]
-                 ;[agalmic/data "0.7.8"]]
+                 [uncomplicate/neanderthal "0.31.0"]
                  ; No need to specify slf4j-api, as it’s required by logback.
                  ; These loggers are here mainly to keep Neanderthal quiet.
-                 ;[org.clojure/tools.logging "0.6.0"]
-                 ;[ch.qos.logback/logback-classic "1.2.3"]]
+                 [org.clojure/tools.logging "0.6.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]]
   ; lein with-env-vars repl
   ; Is this working? Specified in $PATH in .bashrc.
   :env-vars {:LD_LIBRARY_PATH "/opt/intel/mkl/lib:/opt/intel/lib:/opt/intel/mkl/lib/intel64:/opt/intel/mkl/lib/intel64_lin"}
