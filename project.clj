3513396bd4c00edfeac4b3c0adabe0c43d134c90
Ati <asharma@jupiter>
2020-08-04 13:22:03 +0100

    bump neanderthal version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject matlib "0.1.7"
+(defproject matlib "0.1.8"
   :description "A Clojure library of optimisation and control theory tools and convenience functions based on Neanderthal."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -17,7 +17,7 @@
   ; jvm-opts required by Neanderthal on JDK > 8.
   :jvm-opts ^:replace ["--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [uncomplicate/neanderthal "0.31.0"]
+                 [uncomplicate/neanderthal "0.34.0"]
                  ; No need to specify slf4j-api, as it’s required by logback.
                  ; These loggers are here mainly to keep Neanderthal quiet.
                  [org.clojure/tools.logging "0.6.0"]
